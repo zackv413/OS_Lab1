14039a0b551dde7acdeecbeef72aafa45057ef0b
mikecmeli <mikecmeli@gmail.com>
2016-02-10 19:09:44 -0500

    I fixed it

--- batch_scheduler.c
+++ batch_scheduler.c
@@ -1,9 +1,37 @@
-
-
-Hello Mike, from zack.
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <string.h>
 
 int main()
 {
-	return 5;
+	pid_t pid;
+	int f;
+	f = fork();
+	pid = getpid();
+	
+	//char* input = NULL;
+	//char* sgl_str = NULL;
+	//scanf("%s", input);
+	//sgl_str = strtok(input," ");
+	
+	
+	if(f != 0)
+	{
+		wait(NULL);
+ 		//waitpid(f);	
+	}
+	else
+	{
+		//char *argv[] = {"/bin/ls", "-l", 0};
+		//char *argv[] = {"ls", NULL, 0};
+		char *argv[] = {"ls", NULL, "pwd", NULL};
+		pid = getpid();
+		//execv(argv[0], argv);
+		execvp(argv[0], argv);
+		kill(pid,SIGKILL);
+	}
+	printf(":D\n");
+	return 0;
 }
-
