806adc8f95cdcb12eb51731fdd7795b3d05dc581
Zachary Valenti <zvalenti@umass.edu>
2016-02-15 11:50:45 +0000

    Added comments, changed from switch to if

--- process_termination.c
+++ process_termination.c
@@ -6,28 +6,35 @@ int main()
 
     int process_id;
 
-    printf("Please enter a process id: \n");
+    printf("Please enter a process id: ");
+
+    //store the user input into process_id
     scanf("%d",&process_id);
 
-    //printf("You entered: %d\n",process_id);
-
+    //use the kill function and store it's return value into kill_return_value
     int kill_return_value = kill( process_id, SIGKILL );
 
+
+    // 0 indicates a valid kill
     if( kill_return_value == 0 )
     {
         /* Succesfully terminated application */
         printf("Successfully killed process: %d\n", process_id);
     }
-    else
+    else 
     {
-        switch( errno )
+        // If there was an error, it will be stored in errno automatically by kill.
+        // ESRCH indicates that there was no process with the user inputted pID.
+        // EPERM indicates that the user tried to kill a process it did not have permission to kill.
+        
+        if( errno == ESRCH )
         {
-            case ESRCH:
-                printf("Unable to find the specefied pID\n");
-                break;
-            case EPERM:
-                printf("You do not have permission to kill this pID\n");
-                break;
+            printf("Unable to find the specefied pID\n");
+        }
+        else if( errno == EPERM )
+        {
+
+            printf("You do not have permission to kill this pID\n");
         }
     }
 
